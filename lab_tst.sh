4b5779904ccb640aaaf002677e41a1bdb542bd15
esath <esa.hulkko@gmail.com>
2017-09-15 15:49:10 +0300

    Update lab_tst.sh

--- lab_tst.sh
+++ lab_tst.sh
@@ -3,7 +3,7 @@
 #Usage: './rm_ipam_data.sh customername vlan'
 #customername is same as routing-instance/vrf-name
 #
-token=`curl -X POST --user admin:TSFcloud http://192.168.112.148/api/api/user/ | grep token | awk -F\" '{print $10}'`
+token=`curl -X POST --user username:password http://192.168.112.148/api/api/user/ | grep token | awk -F\" '{print $10}'`
 
 subnetid=`curl -X GET -H "token: $token" http://192.168.112.148/api/api/sections/4/subnets/ | sed 's/{/\n/g' | grep $1 | awk -F\" '{print $4}'`
 vrfid=`curl -X GET -H "token: $token" http://192.168.112.148/api/api/vrf/ | sed 's/{/\n/g' | grep $1 | awk -F\" '{print $4}'`
